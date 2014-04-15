42cbc5e96d7ce9f9d0ab472e4d75c27c039d4e4e
chhsueh <ihcaod@gmail.com>
2014-04-15 14:35:21 +0800

    加看別的配對效果按怎 丞宏

---
+++ 翻譯研究/對摩西翻譯改通用.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from 翻譯研究.對摩西翻譯改國語斷詞 import 對摩西翻譯改國語斷詞
+
+class 對摩西翻譯改通用(對摩西翻譯改國語斷詞):
+	埠 = None
+	結果檔名 = None
+	試驗檔名 = None
+	
+# mosesserver -f 閩南語斷詞/model/moses.ini --server-port 8201
+# mosesserver -f 國語斷詞組/model/moses.ini --server-port 8202
+# mosesserver -f 閩南語斷詞組/model/moses.ini --server-port 8203
+# mosesserver -f 國語斷詞組閩南語斷詞/model/moses.ini --server-port 8204
+# mosesserver -f 國語斷詞閩南語斷詞組/model/moses.ini --server-port 8205
+if __name__ == '__main__':
+	翻譯研究 = 對摩西翻譯改通用()
+	翻譯研究.埠 = 8201
+	翻譯研究.結果檔名 = '試驗結果-閩南語斷詞.txt'
+	翻譯研究.試驗檔名 = '../語料/試.國語字斷字.txt'
+# 	翻譯研究.埠 = 8202
+# 	翻譯研究.結果檔名 = '試驗結果-國語斷詞組.txt'
+# 	翻譯研究.試驗檔名 = '../語料/試.國語字.txt'
+# 	翻譯研究.埠 = 8203
+# 	翻譯研究.結果檔名 = '試驗結果-閩南語斷詞組.txt'
+# 	翻譯研究.試驗檔名 = '../語料/試.國語字斷字.txt'
+# 	翻譯研究.埠 = 8204
+# 	翻譯研究.結果檔名 = '試驗結果-國語斷詞組閩南語斷詞.txt'
+# 	翻譯研究.試驗檔名 = '../語料/試.國語字.txt'
+# 	翻譯研究.埠 = 8205
+# 	翻譯研究.結果檔名 = '試驗結果-國語斷詞閩南語斷詞組.txt'
+# 	翻譯研究.試驗檔名 = '../語料/試.國語字斷詞.txt'
+	翻譯研究.載入()
+	翻譯研究.試驗()
