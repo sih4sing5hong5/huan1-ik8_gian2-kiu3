357dc3d0764d158c0304825aa58364fe47269ad7
chhsueh <ihcaod@gmail.com>
2014-04-15 11:37:45 +0800

    先共暫時無用著的摃起來 丞宏

--- 翻譯研究/對摩西翻譯改.py
+++ 翻譯研究/對摩西翻譯改.py
@@ -16,10 +16,11 @@ from 臺灣言語工具.斷詞.型音辭典 import 型音辭典
 from 臺灣言語工具.斷詞.動態規劃斷詞 import 動態規劃斷詞
 
 class 對摩西翻譯改:
-	埠='8100'
-	結果檔名='試驗結果.txt'
-	試驗檔名='../語料/試.國語字斷詞.txt'
-	斷字用戶端 = 摩西用戶端('localhost',8101)
+	埠 = 8100
+	結果檔名 = '試驗結果.txt'
+	試驗檔名 = '../語料/試.國語字斷詞.txt'
+	斷字埠 = 8101
+	摩西揀幾个 = 1
 	語料 = None
 	對齊語料對應表 = None
 	對齊片語表 = None
@@ -31,45 +32,48 @@ class 對摩西翻譯改:
 	用戶端 = None
 	def 載入(self):
 		self.語料 = 讀語料()
-		self.對齊語料對應表 = self.語料.產生對齊語料對應表(
-			來源詞='../語料/訓.國語字_訓.閩南語音.trn.src.vcb',
-			目標詞='../語料/訓.國語字_訓.閩南語音.trn.trg.vcb',
-			機率表='../語料/訓.國語字_訓.閩南語音.t3.final')
+# 		self.對齊語料對應表 = self.語料.產生對齊語料對應表(
+# 			來源詞='../語料/訓.國語字_訓.閩南語音.trn.src.vcb',
+# 			目標詞='../語料/訓.國語字_訓.閩南語音.trn.trg.vcb',
+# 			機率表='../語料/訓.國語字_訓.閩南語音.t3.final')
 
-		辭典對應表模型檔名 = '辭典對應表.pickle'
-		if os.path.isfile(辭典對應表模型檔名):
-			辭典對應表模型檔案 = open(辭典對應表模型檔名, 'rb')
-			教育部對應表 = pickle.load(辭典對應表模型檔案)
-			辭典對應表模型檔案.close()
-		else:
-			教育部對應表 = self.語料.產生辭典對應表('../語料/教育部辭典對應詞結果.json')
-			辭典對應表模型檔案 = open(辭典對應表模型檔名, 'wb')
-			pickle.dump(教育部對應表, 辭典對應表模型檔案, protocol=pickle.HIGHEST_PROTOCOL)
-			辭典對應表模型檔案.close()
-		self.辭典對應表, self.字典對應表 = 教育部對應表
-		語言模型檔名 = '語言模型檔名.pickle'
-		if os.path.isfile(語言模型檔名):
-			語言模型檔案 = open(語言模型檔名, 'rb')
-			self.連詞 = pickle.load(語言模型檔案)
-			語言模型檔案.close()
-		else:
-			self.連詞 = self.語料.讀語言模型檔案('../語料/訓.閩南語音.txt')
-			語言模型檔案 = open(語言模型檔名, 'wb')
-			pickle.dump(self.連詞, 語言模型檔案, protocol=pickle.HIGHEST_PROTOCOL)
-			語言模型檔案.close()
-			
-		self.用戶端 = 摩西用戶端('localhost',self.埠)
+# 		辭典對應表模型檔名 = '辭典對應表.pickle'
+# 		if os.path.isfile(辭典對應表模型檔名):
+# 			辭典對應表模型檔案 = open(辭典對應表模型檔名, 'rb')
+# 			教育部對應表 = pickle.load(辭典對應表模型檔案)
+# 			辭典對應表模型檔案.close()
+# 		else:
+# 			教育部對應表 = self.語料.產生辭典對應表('../語料/教育部辭典對應詞結果.json')
+# 			辭典對應表模型檔案 = open(辭典對應表模型檔名, 'wb')
+# 			pickle.dump(教育部對應表, 辭典對應表模型檔案, protocol=pickle.HIGHEST_PROTOCOL)
+# 			辭典對應表模型檔案.close()
+# 		self.辭典對應表, self.字典對應表 = 教育部對應表
 		
-		self.辭典 = 型音辭典(4)
-		for 型, 音陣列 in self.字典對應表.items():
-			for 音 in 音陣列:
-				組物件 = self.__分析器.產生對齊組(型, 音)
-				for 詞物件 in 組物件.內底詞:
-					for 字物件 in 詞物件.內底字:
-						暫=字物件.型
-						字物件.型=字物件.音
-						字物件.音=暫
-					self.辭典.加詞(詞物件)
+		if self.摩西揀幾个 > 1:
+			語言模型檔名 = '語言模型.pickle'
+			if os.path.isfile(語言模型檔名):
+				語言模型檔案 = open(語言模型檔名, 'rb')
+				self.連詞 = pickle.load(語言模型檔案)
+				語言模型檔案.close()
+			else:
+				self.連詞 = self.語料.讀語言模型檔案('../語料/訓.閩南語音.txt')
+				語言模型檔案 = open(語言模型檔名, 'wb')
+				pickle.dump(self.連詞, 語言模型檔案, protocol=pickle.HIGHEST_PROTOCOL)
+				語言模型檔案.close()
+			
+		self.用戶端 = 摩西用戶端('localhost', self.埠)
+		self.斷字用戶端 = 摩西用戶端('localhost', self.斷字埠)
+		
+# 		self.辭典 = 型音辭典(4)
+# 		for 型, 音陣列 in self.字典對應表.items():
+# 			for 音 in 音陣列:
+# 				組物件 = self.__分析器.產生對齊組(型, 音)
+# 				for 詞物件 in 組物件.內底詞:
+# 					for 字物件 in 詞物件.內底字:
+# 						暫 = 字物件.型
+# 						字物件.型 = 字物件.音
+# 						字物件.音 = 暫
+# 					self.辭典.加詞(詞物件)
 	def 試驗(self):
 		self.__分析器 = 拆文分析器()
 		self.__粗胚 = 文章粗胚()
@@ -79,12 +83,12 @@ class 對摩西翻譯改:
 		結果檔案 = open(self.結果檔名, 'w')
 		for 一逝 in self.語料.讀語料檔案(self.試驗檔名)[:]:
 # 			print(一逝)
-			翻譯結果 = self.用戶端.翻譯(一逝, 另外參數={'nbest':1})
+			翻譯結果 = self.用戶端.翻譯(一逝, 另外參數={'nbest':self.摩西揀幾个})
 			全部句 = []
 			for 上好句 in 翻譯結果['nbest']:
 				句物件 = self.__分析器.建立句物件('')
 # 				print(上好句['hyp'])
-				編號=0
+				編號 = 0
 				for 一个詞 in 上好句['hyp'].split():
 # 					print(一个詞)
 					集物件 = self.__分析器.建立集物件('')
@@ -98,15 +102,18 @@ class 對摩西翻譯改:
 					else:
 						集物件 = self.__分析器.建立集物件(一个詞)
 						句物件.內底集.append(集物件)
-					編號+=1
+					編號 += 1
 				全部句.append(句物件)
 			上好分數 = None
 			上好物件 = None
-			for 句物件 in 全部句:
-				結果物件, 結果分數, 詞數 = 標音工具.標音(self.連詞, 句物件)
-				if 上好分數 == None or 上好分數 < 結果分數:
-					上好分數 = 結果分數
-					上好物件 = 結果物件
+			if self.摩西揀幾个 > 1:
+				for 句物件 in 全部句:
+					結果物件, 結果分數, 詞數 = 標音工具.標音(self.連詞, 句物件)
+					if 上好分數 == None or 上好分數 < 結果分數:
+						上好分數 = 結果分數
+						上好物件 = 結果物件
+			else:
+				上好物件 = 全部句[0]
 			print(譀鏡.看型(上好物件, 物件分字符號='-', 物件分詞符號=' '))
 			print(譀鏡.看型(上好物件, 物件分字符號='-', 物件分詞符號=' '), file=結果檔案)
 
