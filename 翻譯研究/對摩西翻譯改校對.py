3749ba3dd5ffe5fde7137a92e97e1cfa77154ad2
chhsueh <ihcaod@gmail.com>
2014-05-06 13:32:44 +0800

    做的一个參數決定愛斷字的功能無 丞宏

--- 翻譯研究/對摩西翻譯改校對.py
+++ 翻譯研究/對摩西翻譯改校對.py
@@ -7,6 +7,7 @@ class 對摩西翻譯改通用(對摩西翻譯改):
 	斷字埠 = None
 	試驗檔名 = None
 	結果檔名 = None
+	愛斷字 = False
 	結果檔名樣版 = '../模型/結.{0:02}.閩南語試驗結果.txt'
 	試驗檔名樣版 = '../模型/試.{0:02}.斷詞.試驗文本.txt'
 	def __init__(self, 號):
@@ -21,7 +22,7 @@ class 對摩西翻譯改通用(對摩西翻譯改):
 # mosesserver -f 國語斷詞組閩南語斷詞/model/moses.ini --server-port 8204
 # mosesserver -f 國語斷詞閩南語斷詞組/model/moses.ini --server-port 8205
 if __name__ == '__main__':
-	if len(sys.argv)==1:
+	if len(sys.argv) == 1:
 		for 擺 in range(1, 21):
 			翻譯研究 = 對摩西翻譯改通用(擺)
 			翻譯研究.載入()
