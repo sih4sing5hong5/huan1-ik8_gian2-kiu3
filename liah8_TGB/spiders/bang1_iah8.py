30d4bfd9d30c02766080fb0b85eaa0a2ee28e1e9
SIDK <Ihc@gmail.com>
2014-05-07 17:35:16 +0800

    加規則 丞宏

--- liah8_TGB/spiders/bang1_iah8.py
+++ liah8_TGB/spiders/bang1_iah8.py
@@ -9,7 +9,16 @@ class DmozSpider(Spider):
         "http://taioan-chouhap.myweb.hinet.net/0_boklok.htm",
         'http://taioanchouhap.pixnet.net/blog/post/177926499',
     ]
+    rules = (
+        # Extract links matching 'category.php' (but not matching 'subsection.php')
+        # and follow links from them (since no callback means follow=True by default).
+        Rule(SgmlLinkExtractor(allow=(
+    '/taioanchouhap.pixnet.net/blog/.+',))),
+
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        Rule(SgmlLinkExtractor(allow=('item\.php',)), callback='parse_item'),
+    )
 
     def parse(self, response):
-        filename = response.url.replace("/",'_')
+        filename = response.url.replace("/", '_')
         open(filename, 'wb').write(response.body)
