b6fd72d397b486319931c3ae411d8b5ac5b7e3f9
chhsueh <ihcaod@gmail.com>
2014-05-08 11:57:34 +0800

    揣全部網址 丞宏

--- liah8_TGB/spiders/bang1_iah8.py
+++ liah8_TGB/spiders/bang1_iah8.py
@@ -1,5 +1,6 @@
 from scrapy.spider import Spider
 from scrapy.selector import Selector
+from scrapy.http import Request
 
 class DmozSpider(Spider):
 	name = "taioanchouhap"
@@ -11,11 +12,15 @@ class DmozSpider(Spider):
 		'http://taioanchouhap.pixnet.net/blog/post/177926499',
 	]
 	
-	def parse(self, response):
-		filename = response.url.replace("/", '_')
-		open(filename, 'wb').write(response.body)
+	def parse_TGB(self, response):
+		print(response.url)
 		
 		sel = Selector(response)
 		for url in sel.xpath('//a/@href').extract():
-			print(url)
+			if url.startswith('/blog/post/'):
+				yield Request(url='http://taioanchouhap.pixnet.net/'+url,
+							callback=self.parse_TGB)
+			elif url.startswith('http://taioanchouhap.pixnet.net/blog/post/'):
+				yield Request(url=url,
+						callback=self.parse_TGB)
 		
