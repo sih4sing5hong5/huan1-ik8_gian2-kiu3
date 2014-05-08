a8ae83bea71c91f005184e585d1a2254b2399bff
chhsueh <ihcaod@gmail.com>
2014-05-08 12:00:03 +0800

    改轉crawl spider 丞宏

--- liah8_TGB/spiders/bang1_iah8.py
+++ liah8_TGB/spiders/bang1_iah8.py
@@ -1,8 +1,10 @@
 from scrapy.spider import Spider
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from scrapy.http import Request
 
-class DmozSpider(Spider):
+class DmozSpider(CrawlSpider):
 	name = "taioanchouhap"
 	allowed_domains = ["taioanchouhap.pixnet.net",
 					   'taioan-chouhap.myweb.hinet.net']
@@ -11,6 +13,13 @@ class DmozSpider(Spider):
 		"http://taioan-chouhap.myweb.hinet.net/0_boklok.htm",
 		'http://taioanchouhap.pixnet.net/blog/post/177926499',
 	]
+	
+	rules = [
+		Rule(
+			SgmlLinkExtractor(allow=[".+/blog/post/.+", ]),
+			callback="parse_items", follow=True
+		),
+    ]
 	
 	def parse_TGB(self, response):
 		print(response.url)
