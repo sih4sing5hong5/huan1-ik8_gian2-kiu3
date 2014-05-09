1117bc0ef9e15acb6fe9fb088b84cc0fce2271bf
Ihc <ihcaoe@gmail.com>
2014-05-09 11:01:25 +0800

    會使掠網頁，毋過愛先確定格式 丞宏

--- liah8_TGB/spiders/bang1_iah8.py
+++ liah8_TGB/spiders/bang1_iah8.py
@@ -17,8 +17,8 @@ class TgbSpider(CrawlSpider):
 	
 	rules = [
 		Rule(
-			SgmlLinkExtractor(allow=[".+/blog/post/.+", ]),
-			callback="parse_items", follow=True
+			SgmlLinkExtractor(allow=[".*/blog/post/177926499.*", ]),
+			callback="parse_TGB", follow=True
 		),
     ]
 	kinn2 = '/blog/post/177926'
@@ -26,16 +26,16 @@ class TgbSpider(CrawlSpider):
 		print(response.url)
 		
 		sel = Selector(response)
-		for url in sel.xpath('//a/@href').extract():
-			if url.startswith(self.kinn2):
-				yield Request(url='http://taioanchouhap.pixnet.net/' + url,
-							callback=self.parse_TGB)
-			elif url.startswith('http://taioanchouhap.pixnet.net' + self.kinn2):
-				yield Request(url=url,
-						callback=self.parse_TGB)
+# 		for url in sel.xpath('//a/@href').extract():
+# 			if url.startswith(self.kinn2):
+# 				yield Request(url='http://taioanchouhap.pixnet.net/' + url,
+# 							callback=self.parse_TGB)
+# 			elif url.startswith('http://taioanchouhap.pixnet.net' + self.kinn2):
+# 				yield Request(url=url,
+# 						callback=self.parse_TGB)
 		if self.kinn2 in response.url:
 			item = Liah8TgbItem()
-			item['title'] = sel.css('li.publish::title()').extract()
-			item['date'] = sel.css('li.publish::text()').extract()
-			item['context'] = sel.css('div.article-content-inner::text()').extract()
+			item['title'] = sel.css('li.publish').extract()
+			item['date'] = sel.css('li.publish').extract()
+			item['context'] = sel.css('div.article-content-inner').extract()
 			yield item
