d4493f50cb2c39f5c5138d30c8175737375f7f65
Ihc <Ihc@gmail.com>
2014-05-13 21:55:25 +0800

    今仔日掠著的資料壓縮起來　丞宏

--- 處理TGB/解析TGB.py
+++ 處理TGB/解析TGB.py
@@ -1,25 +1,25 @@
 import json
 from 處理TGB.提掉網頁標仔工具 import 提掉網頁標仔工具
 from 處理TGB.提掉文章標仔工具 import 提掉文章標仔工具
+import gzip
 
 class 解析TGB:
-    def 讀(self,json檔名):
-        json檔案=open(json檔名)
-        全部=json.loads(json檔案.read())
+    def 讀(self, json檔名):
+        json檔案 = gzip.open(json檔名, 'rt')
+        全部 = json.loads(json檔案.read())
         json檔案.close()
         for 資料 in 全部:
-            標題工具=提掉網頁標仔工具()
+            標題工具 = 提掉網頁標仔工具()
             標題工具.feed(資料['title'][0])
             print(標題工具.結果())
-            時間工具=提掉網頁標仔工具()
+            時間工具 = 提掉網頁標仔工具()
             時間工具.feed(資料['date'][0])
             print(時間工具.結果())
-            文章工具=提掉文章標仔工具()
+            文章工具 = 提掉文章標仔工具()
             文章工具.feed(資料['context'][0])
             print(文章工具.結果())
         print(len(全部))
 
-if __name__=='__main__':
-    TGB=解析TGB()
-    TGB.讀('../原來TGB.json')
-    
+if __name__ == '__main__':
+    TGB = 解析TGB()
+    TGB.讀('../原來TGB.json.gz')
