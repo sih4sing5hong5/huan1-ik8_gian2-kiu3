522aec6df0f6eb0fcb41ca966df5229f6479f4bb
Ihc <Ihc@gmail.com>
2014-05-18 14:12:31 +0800

    第二步：揣出國語句、閩南語句　丞宏

--- 處理TGB/解析TGB.py
+++ 處理TGB/解析TGB.py
@@ -5,7 +5,7 @@ import gzip
 from 分言語.語言判斷用戶端 import 判斷
 
 class 解析TGB:
-	def 讀(self, json檔名, 分數檔名):
+	def 段落字分析(self, json檔名, 分數檔名):
 		json檔案 = gzip.open(json檔名, 'rt')
 		全部 = json.loads(json檔案.read())
 		json檔案.close()
@@ -38,7 +38,39 @@ class 解析TGB:
 		json.dump(分數狀況, 分數檔案)
 		分數檔案.close()
 		print(len(全部))
+	def 國閩分數(self, 分數檔名, 國閩句檔名):
+		分數檔案 = gzip.open(分數檔名, 'rt')
+		全部 = json.load(分數檔案,)
+		分數檔案.close()
+		分數狀況 = []
+
+# 					'網址':資料['url'],
+# 					'標題':標題工具.結果(),
+# 					'發表日':時間工具.結果(),
+# 					'第幾段':第幾段,
+# 					'攏總幾段':len(文章),
+# 					'內容':段.strip(),
+# 					'分數': 分數
+		國語句 = []
+		漢羅句 = []
+		for 資料 in 全部:
+			國語, 教羅, 通用 = 資料['分數']
+			for 一逝 in 資料['內容'].split('\n'):
+				一逝 = 一逝.strip()
+				if 一逝 != '':
+					if 國語 > 0.2 and 教羅 < 0.001:
+						國語句.append(一逝)
+					elif 資料['攏總幾段'] > 1 and 教羅 > 0.1:
+						漢羅句.append(一逝)
+		國閩句檔案 = gzip.open(國閩句檔名, 'wt')
+		json.dump((國語句, 漢羅句), 國閩句檔案)
+		國閩句檔案.close()
+		print(國語句[:5])
+		print(漢羅句[:5])
+		print('\n'.join(國語句[:]),file=open('../國語句.txt','w'))
+		print('\n'.join(漢羅句[:]),file=open('../漢羅句.txt','w'))
 
 if __name__ == '__main__':
 	TGB = 解析TGB()
-	TGB.讀('../原來TGB.json.gz', '../分數.json.gz')
+# 	TGB.段落字分析('../原來TGB.json.gz', '../分數.json.gz')
+	TGB.國閩分數('../分數.json.gz', '../國閩句.json.gz')
