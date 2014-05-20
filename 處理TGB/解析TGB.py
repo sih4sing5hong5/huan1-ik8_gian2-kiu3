00d27c0c998c49b6a1c13478ce2f233df92cb25d
chhsueh <ihcaod@gmail.com>
2014-05-20 15:23:11 +0800

    讀寫檔案協掉 丞宏

--- 處理TGB/解析TGB.py
+++ 處理TGB/解析TGB.py
@@ -3,12 +3,12 @@ from 處理TGB.提掉網頁標仔工具 import 提掉網頁標仔工具
 from 處理TGB.提掉文章標仔工具 import 提掉文章標仔工具
 import gzip
 from 分言語.語言判斷 import 判斷
+from 處理TGB.資料檔 import 資料檔
 
 class 解析TGB:
+	__資料檔=資料檔()
 	def 段落字分析(self, json檔名, 分數檔名):
-		json檔案 = gzip.open(json檔名, 'rt')
-		全部 = json.loads(json檔案.read())
-		json檔案.close()
+		全部 = self.__資料檔.讀(json檔名)
 		分數狀況 = []
 		for 資料 in 全部:
 			標題工具 = 提掉網頁標仔工具()
@@ -34,14 +34,10 @@ class 解析TGB:
 					})
 				第幾段 += 1
 # 			print(分數狀況[-1])
-		分數檔案 = gzip.open(分數檔名, 'wt')
-		json.dump(分數狀況, 分數檔案)
-		分數檔案.close()
+		self.__資料檔.寫(分數檔名, 分數狀況)
 		print(len(全部))
 	def 分國閩句(self, 分數檔名, 國閩句檔名):
-		分數檔案 = gzip.open(分數檔名, 'rt')
-		全部 = json.load(分數檔案,)
-		分數檔案.close()
+		全部 = self.__資料檔.讀(分數檔名)
 		國語句 = []
 		漢羅句 = []
 		for 資料 in 全部:
@@ -52,29 +48,23 @@ class 解析TGB:
 					if 國語 > 0.2 and 教羅 < 0.001:
 						國語句.append(一逝)
 					elif 資料['攏總幾段'] > 1 and 教羅 > 0.1:
-						if 判斷.有偌濟音標(一逝)[0]>0.1:
+						if 判斷.有偌濟音標(一逝)[0] > 0.1:
 							漢羅句.append(一逝)
-		國閩句檔案 = gzip.open(國閩句檔名, 'wt')
-		json.dump((國語句, 漢羅句), 國閩句檔案)
-		國閩句檔案.close()
-	def 國閩分數(self, 國閩句檔名,分數檔名 ):
-		國閩句檔案 = gzip.open(國閩句檔名, 'rt')
-		國語句, 漢羅句 = json.load(國閩句檔案,)
-		國閩句檔案.close()
-		問題=[]
-		答案=[]
-		for 一逝 in 國語句:
-			問題.append(判斷.閩南語相關分數(一逝))
+		self.__資料檔.寫(國閩句檔名, (國語句, 漢羅句))
+	def 國閩分數(self, 國閩句檔名, 分數檔名):
+		國語句, 漢羅句 = self.__資料檔.讀(國閩句檔名)
+		問題 = []
+		答案 = []
+		for 一逝 in 國語句[:5]:
+			問題.append(判斷.分數(一逝))
 			答案.append(0)
-		for 一逝 in 漢羅句:
-			問題.append(判斷.閩南語相關分數(一逝))
+		for 一逝 in 漢羅句[:5]:
+			問題.append(判斷.分數(一逝))
 			答案.append(1)
-		分數檔案 = gzip.open(分數檔名, 'wt')
-		json.dump((問題, 答案), 分數檔案)
-		分數檔案.close()
+		self.__資料檔.寫(分數檔名, (問題, 答案))
 
 if __name__ == '__main__':
 	TGB = 解析TGB()
 # 	TGB.段落字分析('../語料/TGB/原來TGB.json.gz', '../語料/TGB/分數.json.gz')
 # 	TGB.分國閩句('../語料/TGB/分數.json.gz', '../語料/TGB/國閩句.json.gz')
-# 	TGB.國閩分數('../語料/TGB/國閩句.json.gz','../語料/TGB/逐句訓練分數.json.gz')
+	TGB.國閩分數('../語料/TGB/國閩句.json.gz', '../語料/TGB/逐句訓練分數.json.gz')
