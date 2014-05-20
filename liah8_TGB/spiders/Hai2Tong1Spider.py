9185a5032aa9f0968d130a1ac8b4c476b87abbb4
Ihc <Ihc@gmail.com>
2014-05-20 22:07:51 +0800

    海東國小大部份是全漢的　丞宏

--- liah8_TGB/spiders/Hai2Tong1Spider.py
+++ liah8_TGB/spiders/Hai2Tong1Spider.py
@@ -12,25 +12,26 @@ class Hai2Tong1Spider(CrawlSpider):
 	allowed_domains = ["web.htps.tn.edu.tw",
 					]
 	start_urls = [
- 		'http://web.htps.tn.edu.tw/90ct/Default.htm',
-# 		'http://web.htps.tn.edu.tw/90ct/songs/songs.htm',
-# 		'http://web.htps.tn.edu.tw/90ct/songs/song_l.htm',
-# 		'http://web.htps.tn.edu.tw/90ct/songs/song_m/song_m03_1.htm',
-# 		'http://web.htps.tn.edu.tw/90ct/history/history4.htm',
-# 		'http://web.htps.tn.edu.tw/90ct/main.htm',
-# 		'',
-# 		'',
-# 		'',
-# 		'',
-# 		'',
+  		'http://web.htps.tn.edu.tw/90ct/Default.htm',
+		'http://web.htps.tn.edu.tw/90ct/main.htm',
+		'http://web.htps.tn.edu.tw/90ct/songs/songs.htm',
+		'http://web.htps.tn.edu.tw/90ct/songs/song_l.htm',
+		'http://web.htps.tn.edu.tw/90ct/songs/song_m/song_m03_1.htm',
+		'http://web.htps.tn.edu.tw/90ct/history/history4.htm',
+		'http://web.htps.tn.edu.tw/90ct/double_voice/double_voice1.htm',
+		'http://web.htps.tn.edu.tw/90ct/tlpa/index.htm',
+		'http://web.htps.tn.edu.tw/90ct/strange_words/strange_words.htm',
+		'http://web.htps.tn.edu.tw/90ct/guess_what/guess1.htm',
+		'http://web.htps.tn.edu.tw/90ct/guess_what/guess1.htm',
 	]
 	
 	rules = [
 		Rule(
-			SgmlLinkExtractor(allow=['web.htps.tn.edu.tw/90ct/', ],
-# 							tags=('a', 'area', 'frame'), attrs=('href','src'),
-							),
-			
+			SgmlLinkExtractor(
+				allow=['web.htps.tn.edu.tw/90ct/', ],
+				deny=['web.htps.tn.edu.tw/90ct/default_1.htm'],
+# 				tags=('a', 'area', 'frame'), attrs=('href','src'),
+				),
 			callback="parse_bang7_iah8", follow=True
 		),
 # 		Rule(
@@ -44,6 +45,6 @@ class Hai2Tong1Spider(CrawlSpider):
 		item['url'] = response.url
 		item['title'] = ''
 		item['date'] = ''
-# 		item['context'] = bytearray(response.body[:]).decode('big5',errors='replace')
+		item['context'] = bytearray(response.body[:]).decode('big5',errors='replace')
 # 		print(item['context'])
 		yield item
