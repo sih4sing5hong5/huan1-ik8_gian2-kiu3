9fceca476fa90176e3dc01f5aba96a49fe7c3fb6
chhsueh <ihcaod@gmail.com>
2014-06-12 09:18:26 +0800

    使用算好的分數 丞宏

--- 處理TGB/解析TGB.py
+++ 處理TGB/解析TGB.py
@@ -4,6 +4,7 @@ from 處理TGB.提掉文章標仔工具 import 提掉文章標仔工具
 import gzip
 from 分言語.語言判斷 import 判斷
 from 處理TGB.資料檔 import 資料檔
+import os
 
 class 解析TGB:
 	__資料檔 = 資料檔()
@@ -51,18 +52,36 @@ class 解析TGB:
 						if 判斷.有偌濟音標(一逝)[0] > 0.1:
 							漢羅句.append(一逝)
 		self.__資料檔.寫(國閩句檔名, (國語句, 漢羅句))
-	def 國閩分數(self, 國閩句檔名, 分數檔名, 海東檔名):
+	def 國閩分數(self,句對應分數檔名, 國閩句檔名, 分數檔名, 海東檔名):
 		國語句, 漢羅句 = self.__資料檔.讀(國閩句檔名)
+		if os.path.isfile(句對應分數檔名):
+			句對應分數 =self.__資料檔.讀(句對應分數檔名)
+		else:
+			句對應分數 = {}
 		問題 = []
 		答案 = []
 		for 一逝 in 國語句[:]:
-			問題.append(判斷.分數(一逝))
+			if 一逝 in 句對應分數:
+				分數=句對應分數[一逝]
+				print('cache')
+			else:
+				分數=判斷.分數(一逝)
+				print('query',len(分數))
+			問題.append(分數)
 			答案.append(0)
 		for 一逝 in 漢羅句[:]:
-			問題.append(判斷.分數(一逝))
+			if 一逝 in 句對應分數:
+				分數=句對應分數[一逝]
+			else:
+				分數=判斷.分數(一逝)
+			問題.append(分數)
 			答案.append(1)
 		for 一逝 in self.海東分析(海東檔名)[:]:
-			問題.append(判斷.分數(一逝))
+			if 一逝 in 句對應分數:
+				分數=句對應分數[一逝]
+			else:
+				分數=判斷.分數(一逝)
+			問題.append(分數)
 			答案.append(2)
 		self.__資料檔.寫(分數檔名, (問題, 答案))
 
@@ -89,7 +108,8 @@ if __name__ == '__main__':
 	TGB = 解析TGB()
 # 	TGB.段落字分析('../語料/TGB/原來TGB.json.gz', '../語料/TGB/分數.json.gz')
 # 	TGB.分國閩句('../語料/TGB/分數.json.gz', '../語料/TGB/國閩句.json.gz')
-	TGB.國閩分數('../語料/TGB/國閩句.json.gz',
+	TGB.國閩分數('../語料/TGB/全部句分數.json.gz',
+			'../語料/TGB/國閩句.json.gz',
 			'../語料/TGB/逐句訓練分數.json.gz',
 			'../語料/TGB/原來Hai2Tong1.json.gz')
 # 	TGB.海東分析('../語料/TGB/原來Hai2Tong1.json.gz')
