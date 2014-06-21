a4335cc0f462950b09409986b06dd1443ead26b0
Ihc <Ihc@gmail.com>
2014-06-21 23:29:12 +0800

    PCA效果比LDA閣較好 丞宏

--- 處理TGB/訓練模型.py
+++ 處理TGB/訓練模型.py
@@ -5,6 +5,7 @@ from 處理TGB.解析TGB import 解析TGB
 import numpy as np
 from sklearn import svm
 from 翻譯研究.讀語料 import 讀語料
+from sklearn.decomposition.pca import PCA
 
 class 訓練模型:
 	_資料檔 = 資料檔()
@@ -41,6 +42,17 @@ class 訓練模型:
 		lda.fit(問題, 答案)
 		print('訓練了')
 		return lambda 問:lda.predict(問)
+	def PCA佮SVM模型(self, 問題, 答案):
+		sample_weight_constant = np.ones(len(問題))
+		clf = svm.SVC(C = 1)
+		pca = PCA(n_components = 100)
+# 		clf = svm.NuSVC()
+		print('訓練PCA')
+		pca.fit(問題)
+		print('訓練SVM')
+		clf.fit(pca.transform(問題), 答案, sample_weight = sample_weight_constant)
+		print('訓練了')
+		return lambda 問:clf.predict(pca.transform(問))
 	def LDA佮SVM模型(self, 問題, 答案):
 		sample_weight_constant = np.ones(len(問題))
 		clf = svm.SVC(C = 1)
@@ -151,8 +163,10 @@ if __name__ == '__main__':
 		'../語料/TGB/分國閩/訓.閩南.gz')
 	for 定用詞 in [0, 10, 20, 50, 100, 200, 500, 1000]:
 		訓練問題 = TGB.調整問題(問題, 定用詞)
-		for 名, 方法 in zip(['SVM', 'LDA', 'LDA佮SVM'],
-				[TGB.SVM模型, TGB.LDA模型, TGB.LDA佮SVM模型]):
+		for 名, 方法 in zip(['SVM', 'LDA', 'LDA佮SVM', 'PCA佮SVM'],
+				[TGB.SVM模型, TGB.LDA模型, TGB.LDA佮SVM模型, TGB.PCA佮SVM模型]):
+			if 名 != 'PCA佮SVM':
+				continue
 			試驗函式 = 方法(訓練問題, 答案)
 			print('試驗函式', 名, '定用詞', 定用詞)
 			國毋著, 國全部 = TGB.試驗著毋著(試驗函式, '../語料/TGB/分國閩/試.國語.gz', 0, 定用詞)
