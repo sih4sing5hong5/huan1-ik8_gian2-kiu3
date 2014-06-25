2b2377e73a6b5010dfc38467e6502a79d85ee33d
Ihc <Ihc@gmail.com>
2014-06-26 01:06:55 +0800

    補對齊介面　丞宏

---
+++ 對齊TGB/翻譯評分對齊介面.py
@@ -0,0 +1,27 @@
+
+from bleualign import Aligner
+
+class 翻譯評分對齊介面:
+	公家參數 = {}
+	def __init__(self):
+		self.公家參數 = {}
+		self.公家參數['factored'] = False
+		self.公家參數['filter'] = None
+		self.公家參數['filterthreshold'] = 90
+		self.公家參數['filterlang'] = None
+		self.公家參數['eval'] = None
+		self.公家參數['galechurch'] = None
+		self.公家參數['verbosity'] = 1
+		self.公家參數['printempty'] = False
+		self.公家參數['output'] = None
+	def 對齊(self, 原來, 目標, 原來翻目標, 目標翻原來, 原來對齊, 目標對齊):
+		參數 = self.公家參數.copy()
+		參數['srcfile'] = 原來
+		參數['targetfile'] = 目標
+		參數['srctotarget'] = 原來翻目標
+		參數['targettosrc'] = 目標翻原來
+		參數['output-src'] = 原來對齊
+		參數['output-target'] = 目標對齊
+
+		a = Aligner(參數)
+		a.mainloop()
