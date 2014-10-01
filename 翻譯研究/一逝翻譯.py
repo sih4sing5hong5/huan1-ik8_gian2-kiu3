7078756642f112f3de77ed947239b227f4ba816d
薛丞宏 <ihcaoe@gmail.com>
2014-10-01 21:10:48 +0800

    配合用戶端改介面 丞宏

--- 翻譯研究/一逝翻譯.py
+++ 翻譯研究/一逝翻譯.py
@@ -1,20 +1,22 @@
 from 臺灣言語工具.翻譯.摩西工具.語句編碼器 import 語句編碼器
 import sys
 from 臺灣言語工具.翻譯.摩西工具.摩西用戶端 import 摩西用戶端
-from 翻譯研究.斷詞斷字翻譯 import 斷詞斷字翻譯
 from 臺灣言語工具.解析整理.物件譀鏡 import 物件譀鏡
+from 臺灣言語工具.翻譯.斷詞斷字翻譯 import 斷詞斷字翻譯
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 
 class 一逝翻譯:
 	編碼器 = 語句編碼器()
 	_斷詞斷字翻譯 = 斷詞斷字翻譯()
 	_譀鏡 = 物件譀鏡()
+	_分析器 = 拆文分析器()
 	def __init__(self, 斷詞埠, 斷字埠):
-		self.斷詞用戶端 = 摩西用戶端('localhost', 斷詞埠)
-		self.斷字用戶端 = 摩西用戶端('localhost', 斷字埠)
+		self.斷詞用戶端 = 摩西用戶端('localhost', 斷詞埠, 編碼器=self.編碼器)
+		self.斷字用戶端 = 摩西用戶端('localhost', 斷字埠, 編碼器=self.編碼器)
 	def 譯(self, 一句):
-		句物件 = self._斷詞斷字翻譯.譯一句(
-			self.斷詞用戶端, self.斷字用戶端, self.編碼器,
-				一句)[0]
+		原本句物件 = self._分析器.轉做句物件(一句)
+		句物件 = self._斷詞斷字翻譯.譯(
+			self.斷詞用戶端, self.斷字用戶端, 原本句物件)
 		return self._譀鏡.看分詞(句物件)
 
 if __name__ == '__main__':
