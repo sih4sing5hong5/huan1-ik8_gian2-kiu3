dde1a797e4517e2465c7786d367058b48e70181a
薛丞宏 <ihcaoe@gmail.com>
2014-10-22 12:25:42 +0800

    辭典條目嘛加入連詞，希望伊較會揀著辭典條目 因為辭典是常用詞詞典 所以加詞條合理 丞宏

--- 分言語/閩南語辭典連詞有例句.py
+++ 分言語/閩南語辭典連詞有例句.py
@@ -16,14 +16,15 @@ class 閩南語辭典連詞:
 		SRILM安裝路徑 = '/usr/local/srilm/bin/i686-m64/'
 		語句連詞訓練 = 斯里語句連詞訓練()
 		連詞檔 = 語句連詞訓練.訓練([華臺, 典藏, '../語料/附錄句一對一斷詞.txt.gz',
-					'../語料/辭典一對一.txt.gz'],
+					'../語料/辭典一對一.txt.gz',
+					'../語料/例句一對一.txt.gz',],
 				暫存資料夾, 3, SRILM執行檔路徑=SRILM安裝路徑)
 		return 辭典, 連詞檔
 
 if __name__ == '__main__':
 	辭典, 連詞 = 閩南語辭典連詞().產生()
 	print((辭典, 連詞))
-	閩南語辭典連詞檔案 = gzip.open('../語料/分言語/閩南語辭典連詞.pickle.gz', 'wb')
+	閩南語辭典連詞檔案 = gzip.open('../語料/分言語/閩南語辭典連詞有例句.pickle.gz', 'wb')
 	pickle.dump((辭典, 連詞), 閩南語辭典連詞檔案,
 			protocol=pickle.HIGHEST_PROTOCOL)
 	閩南語辭典連詞檔案.close()
