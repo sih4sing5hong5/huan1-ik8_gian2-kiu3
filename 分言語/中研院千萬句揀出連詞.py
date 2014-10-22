950faafcd7672da35c73195958ed06e8e870f35e
薛丞宏 <ihcaoe@gmail.com>
2014-10-22 11:43:08 +0800

    華語嘛用肯連詞 丞宏

--- 分言語/中研院千萬句揀出連詞.py
+++ 分言語/中研院千萬句揀出連詞.py
@@ -2,12 +2,11 @@ import re
 from 翻譯研究.讀語料 import 讀語料
 import os
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from 臺灣言語工具.表單.實際語句連詞 import 實際語句連詞
-import pickle
 from 臺灣言語工具.斷詞.中研院工具.官方斷詞剖析工具 import 官方斷詞剖析工具
 from 臺灣言語工具.斷詞.中研院工具.斷詞結構化工具 import 斷詞結構化工具
 from 臺灣言語工具.解析整理.物件譀鏡 import 物件譀鏡
-import gzip
+from 臺灣言語工具.表單.斯里語句連詞訓練 import 斯里語句連詞訓練
+import shutil
 
 class 中研院千萬句揀出連詞:
 	揣標題 = re.compile('<title>(.*?)</title>')
@@ -18,14 +17,23 @@ class 中研院千萬句揀出連詞:
 	__斷詞剖析工具 = 官方斷詞剖析工具()
 	__斷詞結構化工具 = 斷詞結構化工具()
 	__譀鏡 = 物件譀鏡()
-	def 處理全部檔案(self, 連詞, 目錄):
+	def 處理全部檔案(self, 目錄, 上尾檔案所在):
+		連詞資料 = []
 		for (所在, 資料夾, 檔案) in os.walk(目錄):
 			for 檔名 in 檔案:
 				if 檔名.endswith('.xml'):
 					檔案 = open(所在 + '/' + 檔名)
 					逐逝 = self.揀(檔案.read())
 					檔案.close()
-					self.加(連詞, 逐逝)
+					self.加(連詞資料, 逐逝)
+		暫存資料夾 = '../語料/校對暫存/算華語連詞'
+		SRILM安裝路徑 = '/usr/local/srilm/bin/i686-m64/'
+		os.makedirs(暫存資料夾, exist_ok=True)
+		資料檔 = os.path.join(暫存資料夾, '資料.txt')
+		self.__語料.寫語料檔案(資料檔, '\n'.join(連詞資料))
+		語句連詞訓練 = 斯里語句連詞訓練()
+		模型檔 = 語句連詞訓練.訓練([資料檔], 暫存資料夾, 連紲詞長度=3, SRILM執行檔路徑=SRILM安裝路徑)
+		shutil.copy(模型檔, 上尾檔案所在)
 	def 揀(self, 文章):
 		逐逝 = []
 # 		for 一逝 in self.揣標題.split(文章)[1::2]:
@@ -43,19 +51,10 @@ class 中研院千萬句揀出連詞:
 		for 一逝 in self.分句.split(文章)[1::2]:
 			逐逝.append(self.莫詞性.sub('', 一逝))
 		return 逐逝
-	def 加(self, 連詞, 逐逝):
+	def 加(self, 連詞資料, 逐逝):
 		for 一句 in 逐逝:
-			組物件 = self.__分析器.建立組物件('')
-			for 詞 in 一句.replace('-',' - ').split('\u3000'):
-				詞物件 = self.__分析器.建立詞物件(詞)
-				組物件.內底詞.append(詞物件)
-# 			print(組物件)
-			連詞.看(組物件)
+			連詞資料.append(' '.join(一句.replace('-', ' - ').split('\u3000')))
 
 if __name__ == '__main__':
-	連詞 = 實際語句連詞(3)
-	中研院千萬句揀出連詞().處理全部檔案(連詞, '/dev/shm/1000萬(XML)/')
-	中研院連詞檔案 = gzip.open('../語料/分言語/中研院連詞.pickle.gz', 'wb')
-	pickle.dump(連詞, 中研院連詞檔案,
-			protocol = pickle.HIGHEST_PROTOCOL)
-	中研院連詞檔案.close()
+	連詞 = 中研院千萬句揀出連詞().處理全部檔案(
+		'/dev/shm/1000萬(XML)/', '../語料/分言語/中研院連詞.lm')
--- 分言語/語言判斷模型.py
+++ 分言語/語言判斷模型.py
@@ -10,10 +10,11 @@ from 臺灣言語工具.斷詞.拄好長度辭典揣詞 import 拄好長度辭�
 import Pyro4
 from 臺灣言語工具.解析整理.文章粗胚 import 文章粗胚
 from 臺灣言語工具.解析整理.轉物件音家私 import 轉物件音家私
+from 臺灣言語工具.表單.肯語句連詞 import 肯語句連詞
 # 國語需要10G記憶體，載入愛4分鐘
 # 閩南語需要5G，愛1分鐘
 class 語言判斷模型:
-	國語連詞 = None
+	華語連詞 = None
 	閩南語辭典 = None
 	閩南語連詞 = None
 	__粗胚 = 文章粗胚()
@@ -24,17 +25,16 @@ class 語言判斷模型:
 	__斷詞結構化工具 = 斷詞結構化工具()
 	__標音 = 連詞揀集內組()
 	__斷詞 = 拄好長度辭典揣詞()
-	def 載入(self, 國語連詞檔名, 閩南語辭典連詞檔名):
-		if os.path.isfile(國語連詞檔名):
-			辭典連詞檔案 = gzip.open(國語連詞檔名, 'rb')
-			self.國語連詞 = pickle.load(辭典連詞檔案)
-			辭典連詞檔案.close()
+	def 載入(self, 華語連詞檔名, 閩南語辭典連詞檔名):
+		if os.path.isfile(華語連詞檔名):
+			self.華語連詞 = 肯語句連詞(華語連詞檔名)
 		if os.path.isfile(閩南語辭典連詞檔名):
 			閩南語辭典連詞檔案 = gzip.open(閩南語辭典連詞檔名, 'rb')
-			self.閩南語辭典, self.閩南語連詞 = pickle.load(閩南語辭典連詞檔案)
+			self.閩南語辭典, 閩南語連詞檔 = pickle.load(閩南語辭典連詞檔案)
 			閩南語辭典連詞檔案.close()
+			self.閩南語連詞 = 肯語句連詞(閩南語連詞檔)
 	def 國語分數(self, 章物件):
-		標好, 分數, 詞數 = self.__標音.標音(self.國語連詞, 章物件)
+		標好, 分數, 詞數 = self.__標音.標音(self.華語連詞, 章物件)
 		return 標好, 分數, 詞數
 	def 閩南語分數(self, 標準組物件):
 		斷好, 分數, 詞數 = self.__斷詞.揣詞(self.閩南語辭典, 標準組物件)
@@ -42,7 +42,7 @@ class 語言判斷模型:
 		return 標好, 分數, 詞數
 	def 孤詞表(self):
 		國語孤詞 = []
-		for 詞, 機率 in self.國語連詞.連詞表.items():
+		for 詞, 機率 in self.華語連詞.連詞表.items():
 			if len(詞) == 1:
 				國語孤詞.append((詞[0], 機率))
 		閩南語孤詞 = []
@@ -52,7 +52,7 @@ class 語言判斷模型:
 		return 國語孤詞, 閩南語孤詞,
 if __name__ == '__main__':
 	判斷模型 = 語言判斷模型()
-	判斷模型.載入('../語料/分言語/中研院連詞.pickle.gz', '../語料/分言語/閩南語辭典連詞.pickle.gz')
+	判斷模型.載入('../語料/分言語/中研院連詞.lm', '../語料/分言語/閩南語辭典連詞.pickle.gz')
 	Pyro4.config.SERIALIZERS_ACCEPTED.add('pickle')
 	Pyro4.Daemon.serveSimple(
 	{
