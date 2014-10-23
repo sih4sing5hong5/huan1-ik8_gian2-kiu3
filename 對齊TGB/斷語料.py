a1e6d21a90bca63cfdf62ca52f29165a483e1498
薛丞宏 <ihcaoe@gmail.com>
2014-10-23 10:55:45 +0800

    華語佮閩南語分開 丞宏

--- 對齊TGB/斷語料.py
+++ 對齊TGB/斷語料.py
@@ -7,7 +7,7 @@ from 臺灣言語工具.解析整理.物件譀鏡 import 物件譀鏡
 from 臺灣言語工具.基本元素.公用變數 import 分詞符號
 import os
 from 臺灣言語工具.解析整理.字物件篩仔 import 字物件篩仔
-from 臺灣言語工具.基本元素.公用變數 import 無音
+import time
 
 class 斷語料:
 	_資料檔 = 資料檔()
@@ -17,16 +17,20 @@ class 斷語料:
 	_判斷 = 判斷
 	_譀鏡 = 物件譀鏡()
 	_篩仔 = 字物件篩仔()
-	def 對齊前處理(self, 編號):
+	def 對齊前處理華語(self, 編號):
 		國語檔案 = '../語料/TGB/分開/{0:04}.國'.format(編號)
 		閩南語檔案 = '../語料/TGB/分開/{0:04}.閩'.format(編號)
 		if os.path.isfile(國語檔案) and os.path.isfile(閩南語檔案):
 			國語斷詞 = []
 			for 國語 in 	self._讀語料.讀語料檔案(國語檔案):
 				物件 = self._判斷.標好國語(國語)
-				國語斷詞.append(self._譀鏡.看型(物件, 物件分詞符號 = 分詞符號))
+				國語斷詞.append(self._譀鏡.看型(物件, 物件分詞符號=分詞符號))
 			self._讀語料.寫語料檔案('../語料/TGB/分開對齊/{0:04}.國'.format(編號),
 						'\n'.join(國語斷詞))
+	def 對齊前處理閩南語(self, 編號):
+		國語檔案 = '../語料/TGB/分開/{0:04}.國'.format(編號)
+		閩南語檔案 = '../語料/TGB/分開/{0:04}.閩'.format(編號)
+		if os.path.isfile(國語檔案) and os.path.isfile(閩南語檔案):
 			閩南語斷詞 = []
 			for 閩南語 in self._讀語料.讀語料檔案(閩南語檔案):
 				處理減號 = self._粗胚.建立物件語句前處理減號(教會羅馬字音標, 閩南語)
@@ -39,5 +43,6 @@ class 斷語料:
 if __name__ == '__main__':
 	TGB = 斷語料()
 	for 編號 in range(1179):
-		print(編號)
-		TGB.對齊前處理(編號)
+		print(編號, time.ctime())
+		TGB.對齊前處理華語(編號)
+		TGB.對齊前處理閩南語(編號)
